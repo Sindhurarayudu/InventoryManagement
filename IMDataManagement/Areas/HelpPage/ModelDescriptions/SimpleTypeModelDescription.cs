efd95e4da72eb1aeebfadd674c22c4779084aaa6
Sindhura <rayudu.sindhura@gmail.com>
2021-04-23 14:37:17 -0700

    Added WebApi

---
+++ IMDataManagement/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace IMDataManagement.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ IMDataManagement/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IMDataManagement.WebApiApplication" Language="C#" %>
